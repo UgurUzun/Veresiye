9c335810eaea2515a3249d134d9510a4e0dcc59f
Section1 <Section1@WISSEN>
2019-06-18 15:03:31 +0300

    frm1

--- Versiye.UI/Program.cs
+++ Versiye.UI/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Autofac;
+using Veresiye.Data;
+using Veresiye.Service;
 
 namespace Versiye.UI
 {
@@ -14,9 +17,35 @@ namespace Versiye.UI
         [STAThread]
         static void Main()
         {
+            //Autofac = IoC
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ApplicationDbContext>().As<ApplicationDbContext>();
+
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+
+            // servislerimiz
+            builder.RegisterType<UserService>().As<IUserService>();
+            builder.RegisterType<CompanyService>().As<ICompanyService>();
+            builder.RegisterType<ActivityService>().As<IActivityService>();
+
+            //Formlarımız
+            builder.RegisterType<FrmMain>().As<FrmMain>();
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
+
+            var container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                var frm = scope.Resolve<FrmMain>();
+                Application.Run(frm);
+            }
         }
     }
 }
